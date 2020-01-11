73438bf3839fef2cc5c12c956adb8fa909b77e23
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-11 22:12:07 +0000

    Fail fast on object definitions, fixes #3

--- munit/src/main/scala/munit/Framework.scala
+++ munit/src/main/scala/munit/Framework.scala
@@ -4,6 +4,7 @@ import sbt.testing.Fingerprint
 
 import com.geirsson.junit.CustomFingerprint
 import com.geirsson.junit.CustomRunners
+import sbt.testing.SubclassFingerprint
 
 class Framework extends com.geirsson.junit.JUnitFramework {
   val munitFingerprint = CustomFingerprint.of(
@@ -11,7 +12,12 @@ class Framework extends com.geirsson.junit.JUnitFramework {
     "munit.FunSuiteRunner"
   )
   override val fingerprints: Array[Fingerprint] = Array(
-    munitFingerprint
+    munitFingerprint,
+    new SubclassFingerprint {
+      def isModule(): Boolean = true
+      def superclassName(): String = "munit.Suite"
+      def requireNoArgConstructor(): Boolean = true
+    }
   )
   override val customRunners: CustomRunners =
     CustomRunners.of(munitFingerprint)
