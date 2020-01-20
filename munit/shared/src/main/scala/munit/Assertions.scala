ed4b40c1b3f46d8292d506c6cbcc463559d342ff
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-20 20:50:52 +0000

    Move DottyBugAssumptionViolatedException to separate file

--- munit/shared/src/main/scala/munit/Assertions.scala
+++ munit/shared/src/main/scala/munit/Assertions.scala
@@ -6,12 +6,6 @@ import munit.internal.difflib.Diffs
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
 
-// FIXME(gabro): Constructing a `org.junit.AssumptionViolatedException` causes Dotty to crash
-// so we use our own Exception class to work around it.
-// See https://github.com/lampepfl/dotty/issues/7990
-class DottyBugAssumptionViolatedException(message: String)
-    extends RuntimeException
-
 object Assertions extends Assertions
 trait Assertions {
 
