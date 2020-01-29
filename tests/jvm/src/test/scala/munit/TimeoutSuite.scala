671d2ec37daa264ae3cad765ca3548a5dd150de9
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-01-29 13:20:17 +0000

    Increase timeout for TimeoutSuite, fixes #31

--- tests/jvm/src/test/scala/munit/TimeoutSuite.scala
+++ tests/jvm/src/test/scala/munit/TimeoutSuite.scala
@@ -6,10 +6,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.FiniteDuration
 
 class TimeoutSuite extends munit.FunSuite {
-  override val munitTimeout: FiniteDuration = Duration(10, "ms")
+  override val munitTimeout: FiniteDuration = Duration(100, "ms")
   test("slow".fail) {
     Future {
-      Thread.sleep(100)
+      Thread.sleep(1000)
     }
   }
   test("fast") {
