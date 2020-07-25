4f9d87a8473af4b7eda70b0a414d3067a74f75d2
Gabriele Petronella <gabriele@buildo.io>
2020-07-25 09:26:51 +0200

    Update test-release.sh to latest dotty

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -9,8 +9,8 @@ coursier resolve \
   org.scalameta:munit_2.11:$version \
   org.scalameta:munit_2.12:$version \
   org.scalameta:munit_2.13:$version \
-  org.scalameta:munit_0.24:$version \
   org.scalameta:munit_0.25:$version \
+  org.scalameta:munit_0.26:$version \
   org.scalameta:munit_native0.4.0-M2_2.11:$version \
   org.scalameta:munit_sjs0.6_2.11:$version \
   org.scalameta:munit_sjs0.6_2.12:$version \
@@ -21,8 +21,8 @@ coursier resolve \
   org.scalameta:munit-scalacheck_2.11:$version \
   org.scalameta:munit-scalacheck_2.12:$version \
   org.scalameta:munit-scalacheck_2.13:$version \
-  org.scalameta:munit-scalacheck_0.24:$version \
   org.scalameta:munit-scalacheck_0.25:$version \
+  org.scalameta:munit-scalacheck_0.26:$version \
   org.scalameta:munit-scalacheck_native0.4.0-M2_2.11:$version \
   org.scalameta:munit-scalacheck_sjs0.6_2.11:$version \
   org.scalameta:munit-scalacheck_sjs0.6_2.12:$version \
