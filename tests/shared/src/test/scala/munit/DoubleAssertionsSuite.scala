cfe13d2e8fe81be8c9db0262b6269e12733ee4b4
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2020-09-05 11:56:12 +0200

    Use .fail

--- tests/shared/src/test/scala/munit/DoubleAssertionsSuite.scala
+++ tests/shared/src/test/scala/munit/DoubleAssertionsSuite.scala
@@ -1,44 +1,24 @@
 package munit
 
-class DoubleAssertionsSuite extends BaseSuite {
-  test("Assert Equals NaN Fails") {
-    try {
-      assertEquals(1.234, Double.NaN, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+class DoubleAssertionsFrameworkSuite extends BaseSuite {
+  test("Assert Equals NaN Fails".fail) {
+    assertEquals(1.234, Double.NaN, 0.0)
   }
 
-  test("Assert NaN Equals Fails") {
-    try {
-      assertEquals(Double.NaN, 1.234, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert NaN Equals Fails".fail) {
+    assertEquals(Double.NaN, 1.234, 0.0)
   }
 
   test("Assert NaN Equals NaN") {
     assertEquals(Double.NaN, Double.NaN, 0.0)
   }
 
-  test("Assert Pos Infinity Not Equals Neg Infinity") {
-    try {
-      assertEquals(Double.PositiveInfinity, Double.NegativeInfinity, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert Pos Infinity Not Equals Neg Infinity".fail) {
+    assertEquals(Double.PositiveInfinity, Double.NegativeInfinity, 0.0)
   }
 
-  test("Assert Pos Infinity Not Equals") {
-    try {
-      assertEquals(Double.PositiveInfinity, 1.23, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert Pos Infinity Not Equals".fail) {
+    assertEquals(Double.PositiveInfinity, 1.23, 0.0)
   }
 
   test("Assert Pos Infinity Equals Infinity") {
@@ -49,58 +29,33 @@ class DoubleAssertionsSuite extends BaseSuite {
     assertEquals(Double.NegativeInfinity, Double.NegativeInfinity, 0.0)
   }
 
-  test("All Infinities") {
-    try {
-      assertEquals(
-        Double.PositiveInfinity,
-        Double.NegativeInfinity,
-        Double.PositiveInfinity
-      )
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("All Infinities".fail) {
+    assertEquals(
+      Double.PositiveInfinity,
+      Double.NegativeInfinity,
+      Double.PositiveInfinity
+    )
   }
 
   // And now, the same with floats...
-  test("Assert Equals NaN Fails") {
-    try {
-      assertEquals(1.234f, Float.NaN, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert Equals NaN Fails".fail) {
+    assertEquals(1.234f, Float.NaN, 0.0)
   }
 
-  test("Assert NaN Equals Fails") {
-    try {
-      assertEquals(Float.NaN, 1.234f, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert NaN Equals Fails".fail) {
+    assertEquals(Float.NaN, 1.234f, 0.0)
   }
 
   test("Assert NaN Equals NaN") {
     assertEquals(Float.NaN, Float.NaN, 0.0)
   }
 
-  test("Assert Pos Infinity Not Equals Neg Infinity") {
-    try {
-      assertEquals(Float.PositiveInfinity, Float.NegativeInfinity, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert Pos Infinity Not Equals Neg Infinity".fail) {
+    assertEquals(Float.PositiveInfinity, Float.NegativeInfinity, 0.0)
   }
 
-  test("Assert Pos Infinity Not Equals") {
-    try {
-      assertEquals(Float.PositiveInfinity, 1.23f, 0.0)
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("Assert Pos Infinity Not Equals".fail) {
+    assertEquals(Float.PositiveInfinity, 1.23f, 0.0)
   }
 
   test("Assert Pos Infinity Equals Infinity") {
@@ -111,16 +66,11 @@ class DoubleAssertionsSuite extends BaseSuite {
     assertEquals(Float.NegativeInfinity, Float.NegativeInfinity, 0.0)
   }
 
-  test("All Infinities") {
-    try {
-      assertEquals(
-        Float.PositiveInfinity,
-        Float.NegativeInfinity,
-        Float.PositiveInfinity
-      )
-      fail("unexpected")
-    } catch {
-      case _: FailException =>
-    }
+  test("All Infinities".fail) {
+    assertEquals(
+      Float.PositiveInfinity,
+      Float.NegativeInfinity,
+      Float.PositiveInfinity
+    )
   }
 }
