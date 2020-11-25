3ca8ba8a3c7a92368ad909380ca8378ce80a8837
Gabriele Petronella <gabriele@buildo.io>
2020-11-25 10:20:42 +0100

    Update to Scala 3.0.0-M2

---
+++ munit/shared/src/main/scala-3.0.0-M1/munit/internal/MacroCompat.scala
@@ -0,0 +1,46 @@
+package munit.internal
+
+import munit.Clue
+import munit.Location
+import scala.quoted._
+
+object MacroCompat {
+
+  trait LocationMacro {
+    inline implicit def generate: Location = ${ locationImpl() }
+  }
+
+  def locationImpl()(using qctx: QuoteContext): Expr[Location] = {
+    import qctx.reflect.{_, given}
+    val path = rootPosition.sourceFile.jpath.toString
+    val startLine = rootPosition.startLine + 1
+    '{ new Location(${Expr(path)}, ${Expr(startLine)}) }
+  }
+
+  trait ClueMacro {
+    inline implicit def generate[T](value: T): Clue[T] = ${ clueImpl('value) }
+  }
+
+  def clueImpl[T:Type](value: Expr[T])(using qctx: QuoteContext): Expr[Clue[T]] = {
+    import qctx.reflect.{_, given}
+    val source = value.unseal.pos.sourceCode
+    val valueType = implicitly[scala.quoted.Type[T]].show
+    '{ new Clue(${Expr(source)}, $value, ${Expr(valueType)}) }
+  }
+
+  trait CompileErrorMacro {
+    inline def compileErrors(inline code: String): String = {
+      val errors = scala.compiletime.testing.typeCheckErrors(code)
+      errors.map { error =>
+        val indent = " " * (error.column - 1)
+        val trimMessage = error.message.linesIterator.map { line =>
+          if (line.matches(" +")) ""
+          else line
+        }.mkString("\n")
+        val separator = if (error.message.contains('\n')) "\n" else " "
+        s"error:${separator}${trimMessage}\n${error.lineContent}\n${indent}^"
+      }.mkString("\n")
+    }
+  }
+
+}
