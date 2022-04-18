03d9322fdb7ead2e957f5cdc7c5bdf02ed876cd3
Andrew Valencik <andrew.valencik@gmail.com>
2022-04-18 10:48:55 -0400

    Run scalafmt 3.5.2

--- munit/shared/src/main/scala-3/munit/internal/MacroCompat.scala
+++ munit/shared/src/main/scala-3/munit/internal/MacroCompat.scala
@@ -17,36 +17,41 @@ object MacroCompat {
     val pos = Position.ofMacroExpansion
     val path = pos.sourceFile.jpath.toString
     val startLine = pos.startLine + 1
-    '{ new Location(${Expr(path)}, ${Expr(startLine)}) }
+    '{ new Location(${ Expr(path) }, ${ Expr(startLine) }) }
   }
 
   trait ClueMacro {
     inline implicit def generate[T](value: T): Clue[T] = ${ clueImpl('value) }
-    implicit def generate[T](value: T): Clue[T] = macro MacroCompatScala2.clueImpl
+    implicit def generate[T](value: T): Clue[T] =
+      macro MacroCompatScala2.clueImpl
   }
 
   def clueImpl[T: Type](value: Expr[T])(using Quotes): Expr[Clue[T]] = {
     import quotes.reflect._
     val source = value.asTerm.pos.sourceCode.getOrElse("")
     val valueType = TypeTree.of[T].show(using Printer.TreeShortCode)
-    '{ new Clue(${Expr(source)}, $value, ${Expr(valueType)}) }
+    '{ new Clue(${ Expr(source) }, $value, ${ Expr(valueType) }) }
   }
-
 
   trait CompileErrorMacro {
     inline def compileErrors(inline code: String): String = {
       val errors = scala.compiletime.testing.typeCheckErrors(code)
-      errors.map { error =>
-        val indent = " " * (error.column - 1)
-        val trimMessage = error.message.linesIterator.map { line =>
-          if (line.matches(" +")) ""
-          else line
-        }.mkString("\n")
-        val separator = if (error.message.contains('\n')) "\n" else " "
-        s"error:${separator}${trimMessage}\n${error.lineContent}\n${indent}^"
-      }.mkString("\n")
+      errors
+        .map { error =>
+          val indent = " " * (error.column - 1)
+          val trimMessage = error.message.linesIterator
+            .map { line =>
+              if (line.matches(" +")) ""
+              else line
+            }
+            .mkString("\n")
+          val separator = if (error.message.contains('\n')) "\n" else " "
+          s"error:${separator}${trimMessage}\n${error.lineContent}\n${indent}^"
+        }
+        .mkString("\n")
     }
-    def compileErrors(code: String): String = macro MacroCompatScala2.compileErrorsImpl
+    def compileErrors(code: String): String =
+      macro MacroCompatScala2.compileErrorsImpl
   }
 
 }
