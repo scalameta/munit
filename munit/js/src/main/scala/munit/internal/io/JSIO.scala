57fe292cc7063e2f553d623cf293ed09fc6b144a
Arman Bilge <armanbilge@gmail.com>
2022-05-27 20:00:21 +0000

    Add support for JSDOM to JSIO

--- munit/js/src/main/scala/munit/internal/io/JSIO.scala
+++ munit/js/src/main/scala/munit/internal/io/JSIO.scala
@@ -6,7 +6,11 @@ import scala.util.Try
 object JSIO {
 
   private def require(module: String): Option[js.Dynamic] = {
-    Try(js.Dynamic.global.require(module)).toOption
+    Try(js.Dynamic.global.require(module)) // Node.js
+      .orElse( // JSDOM
+        Try(js.Dynamic.global.Node.constructor("return require")()(module))
+      )
+      .toOption
   }
   val process: Option[js.Dynamic] = require("process")
   val path: Option[js.Dynamic] = require("path")
