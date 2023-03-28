6797ef57d856a53b1aa9025d37e9d4c50eb5bd2f
wahtique <williamvphan@yahoo.fr>
2023-03-28 19:17:06 +0200

    fix(custom-printer-suite): do not run product tests for 2.12
    copied from [[munit.PrintersSuite]]

--- tests/shared/src/test/scala/munit/CustomPrinterSuite.scala
+++ tests/shared/src/test/scala/munit/CustomPrinterSuite.scala
@@ -3,42 +3,55 @@ package munit
 import org.scalacheck.Prop.forAll
 import munit.internal.console.Printers
 import org.scalacheck.Arbitrary.arbitrary
-import scala.collection.immutable
+import org.scalacheck.Prop
 
 class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
 
   private case class Foo(i: Int)
 
-  private case class Bar(is: List[Int])
+  private case class Bar(l: List[Int])
 
   private case class FooBar(foo: Foo, bar: Bar)
 
   private val genFoo = arbitrary[Int].map(Foo(_))
 
-  private val genBar = arbitrary[List[Int]].map(Bar(_))
+  // limit size to 10 to have a reasonable number of values
+  private val genBar = arbitrary[List[Int]].map(l => Bar(l.take(10)))
 
   private val genFooBar = for {
     foo <- genFoo
     bar <- genBar
   } yield FooBar(foo, bar)
 
-  private val longPrinter = Printer { case l: Long =>
+  private val longPrinter: Printer = Printer.apply { case l: Long =>
     s"MoreThanInt($l)"
   }
 
-  private val fooPrinter = Printer { case Foo(i) =>
+  private val fooPrinter: Printer = Printer.apply { case Foo(i) =>
     s"Foo(INT($i))"
   }
 
-  private val listPrinter = Printer { case l: List[_] =>
+  private val listPrinter: Printer = Printer.apply { case l: List[_] =>
     l.mkString("[", ",", "]")
   }
 
-  private val intPrinter = Printer { case i: Int =>
+  private val intPrinter: Printer = Printer.apply { case i: Int =>
     s"NotNaN($i)"
   }
 
-  property("long") {
+  private val isScala213: Boolean = BuildInfo.scalaVersion.startsWith("2.13")
+
+  private def checkProp(
+      options: TestOptions,
+      isEnabled: Boolean = true
+  )(p: => Prop): Unit = {
+    test(options) {
+      assume(isEnabled, "disabled test")
+      p
+    }
+  }
+
+  checkProp("long") {
     forAll(arbitrary[Long]) { (l: Long) =>
       val obtained = Printers.print(l, longPrinter)
       val expected = s"MoreThanInt($l)"
@@ -46,7 +59,7 @@ class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
     }
   }
 
-  property("list") {
+  checkProp("list") {
     forAll(arbitrary[List[Int]]) { l =>
       val obtained = Printers.print(l, listPrinter)
       val expected = l.mkString("[", ",", "]")
@@ -54,34 +67,31 @@ class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
     }
   }
 
-  property("product") {
-    forAll(arbitrary[Int]) { i =>
-      val input = Foo(i)
-      val obtained = Printers.print(input, fooPrinter)
-      val expected = s"Foo(INT($i))"
+  checkProp("product") {
+    forAll(genFoo) { foo =>
+      val obtained = Printers.print(foo, fooPrinter)
+      val expected = s"Foo(INT(${foo.i}))"
       assertEquals(obtained, expected)
     }
   }
 
-  property("int in product") {
-    forAll(arbitrary[Int]) { i =>
-      val input = Foo(i)
-      val obtained = Printers.print(input, intPrinter)
-      val expected = s"Foo(\n  i = NotNaN($i)\n)"
+  checkProp("int in product", isEnabled = isScala213) {
+    forAll(genFoo) { foo =>
+      val obtained = Printers.print(foo, intPrinter)
+      val expected = s"Foo(\n  i = NotNaN(${foo.i})\n)"
       assertEquals(obtained, expected)
     }
   }
 
-  property("list in product") {
-    forAll(arbitrary[List[Int]]) { l =>
-      val input = Bar(l)
-      val obtained = Printers.print(input, listPrinter)
-      val expected = s"Bar(\n  is = ${l.mkString("[", ",", "]")}\n)"
+  checkProp("list in product", isEnabled = isScala213) {
+    forAll(genBar) { bar =>
+      val obtained = Printers.print(bar, listPrinter)
+      val expected = s"Bar(\n  l = ${bar.l.mkString("[", ",", "]")}\n)"
       assertEquals(obtained, expected)
     }
   }
 
-  property("list and int in product") {
+  checkProp("list and int in product", isEnabled = isScala213) {
     forAll(genFooBar) { foobar =>
       val obtained = Printers
         .print(foobar, listPrinter.orElse(intPrinter))
@@ -94,7 +104,7 @@ class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
             |    i = NotNaN(${foobar.foo.i})
             |  ),
             |  bar = Bar(
-            |    is = ${foobar.bar.is.mkString("[", ",", "]")}
+            |    l = ${foobar.bar.l.mkString("[", ",", "]")}
             |  )
             |)
             |""".stripMargin.stripLeading().stripTrailing()
@@ -102,13 +112,13 @@ class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
     }
   }
 
-  property("all ints in product") {
+  checkProp("all ints in product", isEnabled = isScala213) {
     forAll(genFooBar) { foobar =>
       val obtained = Printers
         .print(foobar, intPrinter)
         .filterNot(_.isWhitespace)
 
-      val expectedbBarList = foobar.bar.is match {
+      val expectedbBarList = foobar.bar.l match {
         case Nil => "Nil"
         case l =>
           l.map(i => s"NotNaN($i)").mkString("List(", ",", ")")
@@ -120,7 +130,7 @@ class CustomPrinterSuite extends FunSuite with ScalaCheckSuite {
             |    i = NotNaN(${foobar.foo.i})
             |  ),
             |  bar = Bar(
-            |    is = $expectedbBarList
+            |    l = $expectedbBarList
             |  )
             |)
             |""".stripMargin.filterNot(_.isWhitespace)
