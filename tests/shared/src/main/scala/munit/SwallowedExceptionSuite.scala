becac1c3c28cd8641362ac3c9b40a9bd6cf8bf8d
Marco Zühlke <mzuehlke@gmail.com>
2023-07-06 09:34:24 +0200

    In case of any Throwable inside a test mark the test as failed.

--- tests/shared/src/main/scala/munit/SwallowedExceptionSuite.scala
+++ tests/shared/src/main/scala/munit/SwallowedExceptionSuite.scala
@@ -6,10 +6,15 @@ class SwallowedExceptionSuite extends FunSuite {
   test("issue-51") {
     throw new Exception("i am not reported") with NoStackTrace
   }
+
+  test("issue-650") {
+    throw new IllegalAccessError("i am reported")
+  }
 }
 object SwallowedExceptionSuite
     extends FrameworkTest(
       classOf[SwallowedExceptionSuite],
       """|==> failure munit.SwallowedExceptionSuite.issue-51 - i am not reported
+         |==> failure munit.SwallowedExceptionSuite.issue-650 - i am reported
          |""".stripMargin
     )
