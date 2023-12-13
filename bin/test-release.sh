c32a10cf9184fca26a60af785ff34f58d4e940e6
Andrew Valencik <andrew.valencik@gmail.com>
2023-12-12 19:12:41 -0500

    Drop 2.11 from test-release.sh

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -6,24 +6,18 @@ argumentsRest=${@:2}
 suffix=${argumentsRest:-}
 
 coursier resolve \
-  org.scalameta:munit_2.11:$version \
   org.scalameta:munit_2.12:$version \
   org.scalameta:munit_2.13:$version \
   org.scalameta:munit_3.0.0:$version \
-  org.scalameta:munit_native0.4_2.11:$version \
   org.scalameta:munit_native0.4_2.12:$version \
   org.scalameta:munit_native0.4_2.13:$version \
-  org.scalameta:munit_sjs1_2.11:$version \
   org.scalameta:munit_sjs1_2.12:$version \
   org.scalameta:munit_sjs1_2.13:$version \
-  org.scalameta:munit-scalacheck_2.11:$version \
   org.scalameta:munit-scalacheck_2.12:$version \
   org.scalameta:munit-scalacheck_2.13:$version \
   org.scalameta:munit-scalacheck_3.0.0:$version \
-  org.scalameta:munit-scalacheck_native0.4_2.11:$version \
   org.scalameta:munit-scalacheck_native0.4_2.12:$version \
   org.scalameta:munit-scalacheck_native0.4_2.13:$version \
-  org.scalameta:munit-scalacheck_sjs1_2.11:$version \
   org.scalameta:munit-scalacheck_sjs1_2.12:$version \
   org.scalameta:munit-scalacheck_sjs1_2.13:$version \
   org.scalameta:munit-docs_2.12:$version \
