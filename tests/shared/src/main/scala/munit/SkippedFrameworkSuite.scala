825c032e36df38f3f5bba2fb7cdc651e342731b5
Marco Zühlke <mzuehlke@gmail.com>
2024-04-18 09:02:17 +0200

    create test to document current status

--- tests/shared/src/main/scala/munit/SkippedFrameworkSuite.scala
+++ tests/shared/src/main/scala/munit/SkippedFrameworkSuite.scala
@@ -23,5 +23,65 @@ object SkippedFrameworkSuite
          |==> ignored munit.SkippedFrameworkSuite.ignore
          |==> success munit.SkippedFrameworkSuite.assume(true)
          |==> skipped munit.SkippedFrameworkSuite.assume(false) - assume it fails
-         |""".stripMargin
+         |""".stripMargin,
+      format = SbtFormat
+    )
+
+object SkippedFrameworkStdoutOtherSuite
+    extends FrameworkTest(
+      classOf[SkippedFrameworkSuite],
+      """|munit.SkippedFrameworkSuite:
+         |  + pass <elapsed time>
+         |  + assume(true) <elapsed time>
+         |""".stripMargin,
+      format = StdoutFormat,
+      tags = Set(NoJVM)
+    )
+
+object SkippedFrameworkStdoutOtherVerboseSuite
+  extends FrameworkTest(
+    classOf[SkippedFrameworkSuite],
+    """|munit.SkippedFrameworkSuite:
+       |pass started
+       |  + pass <elapsed time>
+       |==> i ignore ignored
+       |assume(true) started
+       |  + assume(true) <elapsed time>
+       |assume(false) started
+       |==> s assume(false) skipped
+       |""".stripMargin,
+    format = StdoutFormat,
+    tags = Set(NoJVM),
+    arguments = Array("-v")
+  )
+
+object SkippedFrameworkStdoutJVMSuite
+    extends FrameworkTest(
+      classOf[SkippedFrameworkSuite],
+      """|munit.SkippedFrameworkSuite:
+         |  + pass <elapsed time>
+         |==> i munit.SkippedFrameworkSuite.ignore ignored <elapsed time>
+         |  + assume(true) <elapsed time>
+         |""".stripMargin,
+      format = StdoutFormat,
+      tags = Set(OnlyJVM)
+    )
+
+object SkippedFrameworkStdoutJVMVerboseSuite
+    extends FrameworkTest(
+      classOf[SkippedFrameworkSuite],
+      """|munit.SkippedFrameworkSuite started
+         |munit.SkippedFrameworkSuite:
+         |munit.SkippedFrameworkSuite.pass started
+         |  + pass <elapsed time>
+         |==> i munit.SkippedFrameworkSuite.ignore ignored <elapsed time>
+         |munit.SkippedFrameworkSuite.assume(true) started
+         |  + assume(true) <elapsed time>
+         |munit.SkippedFrameworkSuite.assume(false) started
+         |==> i assume(false)
+         |Test run munit.SkippedFrameworkSuite finished: 0 failed, 1 ignored, 3 total, <elapsed time>
+         |""".stripMargin,
+      format = StdoutFormat,
+      tags = Set(OnlyJVM),
+      arguments = Array("-v")
     )
