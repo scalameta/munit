e8a60f9087fa91d4f0ae329778af4e0b49cdd4c6
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-05-21 07:53:32 -0700

    MunitRunner: use simple Iterator, not LazyList
    There's no need to persist the values in the LazyList when they are not
    going to be reused.

--- munit/shared/src/main/scala-2.13/munit/internal/Compat.scala
+++ munit/shared/src/main/scala-2.13/munit/internal/Compat.scala
@@ -1,8 +1,6 @@
 package munit.internal
 
 object Compat {
-  type LazyList[+T] = scala.LazyList[T]
-  val LazyList = scala.LazyList
   def productElementNames(p: Product): Iterator[String] = p.productElementNames
   def collectionClassName(i: Iterable[_]): String = i
     .asInstanceOf[{ def collectionClassName: String }].collectionClassName
--- munit/shared/src/main/scala-pre-2.13/munit/internal/Compat.scala
+++ munit/shared/src/main/scala-pre-2.13/munit/internal/Compat.scala
@@ -1,8 +1,6 @@
 package munit.internal
 
 object Compat {
-  type LazyList[+T] = Stream[T]
-  val LazyList = scala.Stream
   def productElementNames(p: Product): Iterator[String] = Iterator
     .continually("")
   def collectionClassName(i: Iterable[_]): String = i.stringPrefix
