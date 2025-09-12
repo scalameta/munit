4f94037758b9059e7ddf1ebc33541c4e9447d8c2
Tomasz Godzik <tgodzik@virtuslab.com>
2025-09-12 08:47:29 +0200

    bugfix: Add back FutureCompat to avoid binary compat issues
    The issues might happen if new munit is used and older scalacheck.

---
+++ munit/shared/src/main/scala/munit/internal/FutureCompat.scala
@@ -0,0 +1,27 @@
+package munit.internal
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.util.Try
+
+/**
+ * This class is not used and should not be used, but it's kept
+ * for backward compatibility with munit-scalacheck.
+ */
+@deprecated
+private[munit] object FutureCompat {
+  implicit class ExtensionFuture[T](f: Future[T]) {
+    @deprecated
+    def flattenCompat[S](ec: ExecutionContext)(implicit
+        ev: T <:< Future[S]
+    ): Future[S] = f.flatten
+    @deprecated
+    def transformCompat[B](fn: Try[T] => Try[B])(implicit
+        ec: ExecutionContext
+    ): Future[B] = f.transform(fn)
+    @deprecated
+    def transformWithCompat[B](fn: Try[T] => Future[B])(implicit
+        ec: ExecutionContext
+    ): Future[B] = f.transformWith(fn)
+  }
+}
