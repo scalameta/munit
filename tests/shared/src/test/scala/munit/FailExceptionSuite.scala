7a7463fe37e630435eaf0fd65b01c09720e28654
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-12-23 16:45:45 -0800

    scalafmt: upgrade to v3.10.3

--- tests/shared/src/test/scala/munit/FailExceptionSuite.scala
+++ tests/shared/src/test/scala/munit/FailExceptionSuite.scala
@@ -6,12 +6,14 @@ class FailExceptionSuite extends BaseSuite {
     assert(clue(e).isInstanceOf[Serializable])
   }
 
-  test("assertion-error-no-exception")(try {
-    intercept[AssertionError](println("throwing no exception!"))
-    throw new Exception("should not reach here")
-  } catch {
-    case e: FailException =>
-      assert(e.getMessage.contains("expected exception of type 'java.lang.AssertionError' but body evaluated successfully"))
-    case _: Throwable => fail("No FailException was thrown")
-  })
+  test("assertion-error-no-exception")(
+    try {
+      intercept[AssertionError](println("throwing no exception!"))
+      throw new Exception("should not reach here")
+    } catch {
+      case e: FailException =>
+        assert(e.getMessage.contains("expected exception of type 'java.lang.AssertionError' but body evaluated successfully"))
+      case _: Throwable => fail("No FailException was thrown")
+    }
+  )
 }
