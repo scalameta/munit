dfc1ea64364bf2b47e5c1e49c679814a281ac028
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-03-03 19:58:59 -0800

    Ansi: correct "light" to "bright" colors
    Bright is the correct prefix for these color names.

--- junit-interface/src/main/java/munit/internal/junitinterface/Ansi.java
+++ junit-interface/src/main/java/munit/internal/junitinterface/Ansi.java
@@ -4,7 +4,7 @@ public class Ansi {
   // Standard ANSI sequences
   private static final String NORMAL = "\u001B[0m";
   private static final String HIGH_INTENSITY = "\u001B[1m";
-  private static final String LOW_INTESITY = "\u001B[2m";
+  private static final String LOW_INTENSITY = "\u001B[2m";
   private static final String BLACK = "\u001B[30m";
   private static final String RED = "\u001B[31m";
   private static final String GREEN = "\u001B[32m";
@@ -15,12 +15,12 @@ public class Ansi {
   private static final String WHITE = "\u001B[37m";
 
   private static final String DARK_GREY = "\u001B[90m";
-  private static final String LIGHT_RED = "\u001B[91m";
-  private static final String LIGHT_GREEN = "\u001B[92m";
-  private static final String LIGHT_YELLOW = "\u001B[93m";
-  private static final String LIGHT_BLUE = "\u001B[94m";
-  private static final String LIGHT_MAGENTA = "\u001B[95m";
-  private static final String LIGHT_CYAN = "\u001B[96m";
+  private static final String BRIGHT_RED = "\u001B[91m";
+  private static final String BRIGHT_GREEN = "\u001B[92m";
+  private static final String BRIGHT_YELLOW = "\u001B[93m";
+  private static final String BRIGHT_BLUE = "\u001B[94m";
+  private static final String BRIGHT_MAGENTA = "\u001B[95m";
+  private static final String BRIGHT_CYAN = "\u001B[96m";
 
   private static final boolean noColor = shouldDisableColor();
 
@@ -55,18 +55,18 @@ public class Ansi {
   public static final String SKIPPED = YELLOW;
   public static final String SUCCESS1 = GREEN;
   public static final String SUCCESS2 = GREEN;
-  public static final String ERRCOUNT = LIGHT_RED;
+  public static final String ERRCOUNT = BRIGHT_RED;
   public static final String IGNCOUNT = YELLOW;
-  public static final String WARNMSG = LIGHT_YELLOW;
-  public static final String ERRMSG = LIGHT_RED;
+  public static final String WARNMSG = BRIGHT_YELLOW;
+  public static final String ERRMSG = BRIGHT_RED;
   public static final String NNAME1 = YELLOW;
   public static final String NNAME2 = YELLOW;
   public static final String NNAME3 = YELLOW;
   public static final String ENAME1 = YELLOW;
-  public static final String ENAME2 = LIGHT_RED;
+  public static final String ENAME2 = BRIGHT_RED;
   public static final String ENAME3 = YELLOW;
-  public static final String FAILURE1 = LIGHT_RED;
-  public static final String FAILURE2 = LIGHT_RED;
+  public static final String FAILURE1 = BRIGHT_RED;
+  public static final String FAILURE2 = BRIGHT_RED;
   public static final String BOLD = HIGH_INTENSITY;
   public static final String FAINT = DARK_GREY;
 }
